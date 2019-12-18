007a6b0df4be1b20a7418adc85f18f94c839d5b7
César Pomar <cesarpomar18@gmail.com>
2019-12-18 01:45:27 +0100

    driver health check implementation and major fixes

--- driver-builder/driver-install.sh
+++ driver-builder/driver-install.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+apt update 
+apt -y install openjdk-8-jdk openssl nginx
 echo ${IGNIS_HOME}/lib/native > /etc/ld.so.conf.d/ignis-lib.conf
 ldconfig
-apt update 
-apt -y install openjdk-8-jdk openssl
+#Health check
+cat > /etc/nginx/sites-enabled/default << 'EOL'
+server {
+    listen 8080 default_server;
+
+    location / {
+        access_log off;
+        return 200 "Ok\n";
+    }
+}
+EOL
--- executor-builder/executor-install.sh
+++ executor-builder/executor-install.sh
@@ -3,16 +3,37 @@
 echo ${IGNIS_HOME}/lib/native > /etc/ld.so.conf.d/ignis-lib.conf
 ldconfig
 apt update 
-apt install -y openssh-server
+apt install -y openssh-server curl
 mkdir /var/run/sshd
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
-cd ${IGNIS_HOME}/bin
-{ 
-	echo '#!/bin/bash'; 
-	echo 'export -p >> /etc/profile'; 
-	echo 'mkdir -p ~/.ssh'; 
-	echo 'echo ${DRIVER_PUBLIC_KEY} >> ~/.ssh/authorized_keys'; 
-	echo '/usr/sbin/sshd -D'; 
-} > ignis-sshd  
-chmod +x ignis-sshd
+cat > ${IGNIS_HOME}/bin/ignis-executor << 'EOL'
+#!/bin/bash
+export -p >> /etc/profile
+mkdir -p ~/.ssh
+echo ${IGNIS_DRIVER_PUBLIC_KEY} >> ~/.ssh/authorized_keys
+/usr/sbin/sshd
+
+interval=${IGNIS_DRIVER_HEALTHCHECK_INTERVAL}
+timeout=${IGNIS_DRIVER_HEALTHCHECK_TIMEOUT}
+retries=${IGNIS_DRIVER_HEALTHCHECK_RETRIES}
+url=${IGNIS_DRIVER_HEALTHCHECK_URL}
+attempt=0
+while true; do
+    if [ -f "${IGNIS_HOME}/exit" ]; then
+        exit 0
+    fi
+    if $(curl --output /dev/null --silent --head --fail --connect-timeout ${timeout} ${url}); then
+        attempt=0
+    else
+        if [ ${attempt} -eq ${retries} ];then
+            echo "Driver lost, exiting"
+            exit 1
+        fi
+        attempt=$(($attempt+1))
+    fi
+    sleep ${interval}
+done
+
+EOL
+chmod +x ${IGNIS_HOME}/bin/ignis-executor
