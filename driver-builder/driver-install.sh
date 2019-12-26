0e3c040f2c2e57e87f8a8fa29a11342545bfb948
César Pomar <cesarpomar18@gmail.com>
2019-12-26 05:53:50 +0100

    remove nginx server from driver, now implemented in the backend

--- driver-builder/driver-install.sh
+++ driver-builder/driver-install.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 apt update 
-apt -y install openjdk-8-jdk openssl nginx
+apt -y install openjdk-8-jdk openssl
 echo ${IGNIS_HOME}/lib/native > /etc/ld.so.conf.d/ignis-lib.conf
 ldconfig
-#Health check
-cat > /etc/nginx/sites-enabled/default << 'EOL'
-server {
-    listen 8080 default_server;
-
-    location / {
-        access_log off;
-        return 200 "Ok\n";
-    }
-}
-EOL
