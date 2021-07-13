78a977fc4e1c1d696418c5b6dc9b6baad96e2a28
César Pomar <cesarpomar18@gmail.com>
2021-07-13 04:06:08 +0200

    Dockerfiles for the new build system

---
+++ Dockerfiles/driver-builder/driver-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ldconfig
+export DEBIAN_FRONTEND=noninteractive
+apt update 
+apt -y --no-install-recommends install openjdk-14-jre-headless openssl
+rm -rf /var/lib/apt/lists/*
+
+{ \
+	echo '#!/bin/bash'; \
+	echo 'exec java -cp "${IGNIS_HOME}/lib/java/*" org.ignis.backend.Main "$@"'; \
+} > bin/ignis-backend
+chmod +x bin/ignis-backend
