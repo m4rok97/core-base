ec47b05fa1f549f068938b859d7968f2fbcbe516
Marcos Alderete Flores <m4rok97@gmail.com>
2025-08-20 12:41:30 +0200

    Add ignis-host to binary declaration

--- scheduler-slurm/src/main/java/org/ignis/scheduler/Slurm.java
+++ scheduler-slurm/src/main/java/org/ignis/scheduler/Slurm.java
@@ -38,7 +38,7 @@ public final class Slurm implements IScheduler {
 
     public Slurm(String binary) {
         if (binary == null) {
-            binary = "sbatch";
+            binary = "ignis-host sbatch";
         }
         this.binary = binary;
     }
